00934bb5970bbd331dc3bb82e38f855c817f2c17
alfonsoristorato <alfonsoristorato@gmail.com>
2022-12-30 12:50:23 +0000

    backend boilerplate

---
+++ lucreziasPresent-backend/src/main/java/com/alfonsoristorato/lucreziaspresentbackend/LucreziasPresentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alfonsoristorato.lucreziaspresentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucreziasPresentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LucreziasPresentBackendApplication.class, args);
+    }
+
+}
