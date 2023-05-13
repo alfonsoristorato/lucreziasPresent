8a5c52d6b10e786b76a0387dbd23a9a2b5a9ac7d
alfonsoristorato <alfonsoristorato@gmail.com>
2023-05-13 11:47:53 +0100

    added arg to script to run docker compose detached

--- start-app-for-func-tests.sh
+++ start-app-for-func-tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-docker-compose -f docker-compose.functional.yml up --force-recreate -V
+DETACHED=${1}
+if [ "$1" ]
+then
+  docker-compose -f docker-compose.functional.yml up --force-recreate -V "$DETACHED"
+else
+  docker-compose -f docker-compose.functional.yml up --force-recreate -V
+fi
