84da8938983aa7b28317d450cc5f5e23a22ba211
alfonsoristorato <alfonsoristorato@gmail.com>
2023-05-20 17:34:12 +0100

    CredentialsControllerTest and UserControllerTest refactors

--- lucreziasPresent-backend/src/test/java/com/alfonsoristorato/lucreziaspresentbackend/controller/UserControllerTest.java
+++ lucreziasPresent-backend/src/test/java/com/alfonsoristorato/lucreziaspresentbackend/controller/UserControllerTest.java
@@ -5,7 +5,7 @@ import com.alfonsoristorato.lucreziaspresentbackend.model.ChangeUserRoleDTO;
 import com.alfonsoristorato.lucreziaspresentbackend.model.NewUserRequestDTO;
 import com.alfonsoristorato.lucreziaspresentbackend.model.User;
 import com.alfonsoristorato.lucreziaspresentbackend.service.UserService;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,7 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Objects;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -30,12 +29,13 @@ public class UserControllerTest {
 
     @Test
     void getAllUsersReturnsListOfUsersAndOk_and_callsUserService() {
-        List<User> userList = List.of(new User((long)1,"user", "password", "admin", 0, false));
+        List<User> userList = List.of(new User(1L, "user", "password", "admin", 0, false));
         when(userService.getAllUsers()).thenReturn(userList);
+
         ResponseEntity<List<User>> response = userController.getAllUsers();
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(userList, Objects.requireNonNull(response.getBody())),
-                () -> Assertions.assertEquals(HttpStatus.OK, response.getStatusCode()));
+
+        Assertions.assertThat(response.getBody()).isEqualTo(userList);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         verify(userService).getAllUsers();
     }
 
@@ -43,10 +43,11 @@ public class UserControllerTest {
     void editUserRoleReturnsAStringAndOk_and_callsUserService() {
         ChangeUserRoleDTO changeUserRoleDTO = new ChangeUserRoleDTO("user");
         when(userService.editUserRole(1, changeUserRoleDTO)).thenReturn("User role updated.");
+
         ResponseEntity<String> response = userController.editUserRole(1, changeUserRoleDTO);
-        Assertions.assertAll(
-                () -> Assertions.assertEquals("User role updated.", Objects.requireNonNull(response.getBody())),
-                () -> Assertions.assertEquals(HttpStatus.OK, response.getStatusCode()));
+
+        Assertions.assertThat(response.getBody()).isEqualTo("User role updated.");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         verify(userService).editUserRole(1, changeUserRoleDTO);
     }
 
@@ -54,10 +55,11 @@ public class UserControllerTest {
     void editUserAttemptsReturnsAStringAndOk_and_callsUserService() {
         ChangeUserAttemptsDTO changeUserAttemptsDTO = new ChangeUserAttemptsDTO(0);
         when(userService.editUserAttempts(1, changeUserAttemptsDTO)).thenReturn("User attempts updated.");
+
         ResponseEntity<String> response = userController.editUserAttempts(1, changeUserAttemptsDTO);
-        Assertions.assertAll(
-                () -> Assertions.assertEquals("User attempts updated.", Objects.requireNonNull(response.getBody())),
-                () -> Assertions.assertEquals(HttpStatus.OK, response.getStatusCode()));
+
+        Assertions.assertThat(response.getBody()).isEqualTo("User attempts updated.");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         verify(userService).editUserAttempts(1, changeUserAttemptsDTO);
     }
 
@@ -65,20 +67,22 @@ public class UserControllerTest {
     void addUserReturnsAStringAndOk_and_callsUserService() {
         NewUserRequestDTO newUserRequestDTO = new NewUserRequestDTO("newUserName");
         when(userService.addUser(newUserRequestDTO)).thenReturn("userPassword");
+
         ResponseEntity<String> response = userController.addUser(newUserRequestDTO);
-        Assertions.assertAll(
-                () -> Assertions.assertEquals("userPassword", Objects.requireNonNull(response.getBody())),
-                () -> Assertions.assertEquals(HttpStatus.OK, response.getStatusCode()));
+
+        Assertions.assertThat(response.getBody()).isEqualTo("userPassword");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         verify(userService).addUser(newUserRequestDTO);
     }
 
     @Test
     void resetUserPasswordReturnsAStringAndOk_and_callsUserService() {
         when(userService.resetUserPassword(1)).thenReturn("userNewPassword");
+
         ResponseEntity<String> response = userController.resetUserPassword(1);
-        Assertions.assertAll(
-                () -> Assertions.assertEquals("userNewPassword", Objects.requireNonNull(response.getBody())),
-                () -> Assertions.assertEquals(HttpStatus.OK, response.getStatusCode()));
+
+        Assertions.assertThat(response.getBody()).isEqualTo("userNewPassword");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         verify(userService).resetUserPassword(1);
     }
 
