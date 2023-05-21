4ac5877dc9b467aac3de0192f6c8260df292c46e
alfonsoristorato <alfonsoristorato@gmail.com>
2023-05-21 13:21:53 +0100

    GlobalExceptionHandler tests

---
+++ lucreziasPresent-backend/src/test/java/com/alfonsoristorato/lucreziaspresentbackend/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,62 @@
+package com.alfonsoristorato.lucreziaspresentbackend.exception;
+
+import com.alfonsoristorato.lucreziaspresentbackend.model.EntryError;
+import com.alfonsoristorato.lucreziaspresentbackend.model.UserError;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.stream.Stream;
+
+@ExtendWith(MockitoExtension.class)
+public class GlobalExceptionHandlerTest {
+
+    @InjectMocks
+    private GlobalExceptionHandler globalExceptionHandler;
+
+    @Test
+    void defaultExceptions_catchesAllExceptions(){
+        ResponseEntity<String> response = globalExceptionHandler.defaultExceptions(new Exception("I threw"));
+        Assertions.assertThat(response.getBody()).isEqualTo("I threw");
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ParameterizedTest
+    @MethodSource("userExceptionTestData")
+    void userException_catchesAllUserExceptions_andReturnsExpectedStatusCode(UserError userError, HttpStatus httpStatus){
+        ResponseEntity<UserError> response = globalExceptionHandler.userException(new UserException(userError));
+        Assertions.assertThat(response.getBody()).isEqualTo(userError);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(httpStatus);
+    }
+
+    @ParameterizedTest
+    @MethodSource("entryExceptionTestData")
+    void entryException_catchesAllEntryExceptions_andReturnsExpectedStatusCode(EntryError entryError, HttpStatus httpStatus){
+        ResponseEntity<EntryError> response = globalExceptionHandler.entryException(new EntryException(entryError));
+        Assertions.assertThat(response.getBody()).isEqualTo(entryError);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(httpStatus);
+    }
+
+
+    private static Stream<Arguments> userExceptionTestData(){
+        return Stream.of(
+                Arguments.of(UserError.USER_NOT_FOUND(), HttpStatus.NOT_FOUND),
+                Arguments.of(UserError.DISALLOWED_CHANGE("you can't change this"), HttpStatus.INTERNAL_SERVER_ERROR),
+                Arguments.of(UserError.USER_ERROR("this is an error"), HttpStatus.INTERNAL_SERVER_ERROR)
+        );
+    }
+
+    private static Stream<Arguments> entryExceptionTestData(){
+        return Stream.of(
+                Arguments.of(EntryError.ENTRY_NOT_FOUND(), HttpStatus.NOT_FOUND),
+                Arguments.of(EntryError.ENTRY_ERROR("this is an error"), HttpStatus.INTERNAL_SERVER_ERROR)
+        );
+    }
+}
