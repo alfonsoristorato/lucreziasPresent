4596ec7aca3c2fdc7f6f16979443d1f595284d33
alfonsoristorato <alfonsoristorato@gmail.com>
2023-05-22 13:09:15 +0100

    added further integration tests for login scenarios

--- lucreziasPresent-backend/src/test/java/com/alfonsoristorato/lucreziaspresentbackend/integrationTests/CredentialsTest.java
+++ lucreziasPresent-backend/src/test/java/com/alfonsoristorato/lucreziaspresentbackend/integrationTests/CredentialsTest.java
@@ -1,25 +1,87 @@
 package com.alfonsoristorato.lucreziaspresentbackend.integrationTests;
 
-import org.hamcrest.Matchers;
+import com.alfonsoristorato.lucreziaspresentbackend.model.LoginRequestDTO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Map;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class CredentialsTest extends IntegrationTestsConfig {
+
 
     @Autowired
     private HttpClient client;
 
     @Test
-    public void login() {
-        Map<String, String> body = Map.of("username", "admin", "password", "defaultPass");
+    @DisplayName("Login:: right credentials")
+    public void login_withRightCredentials() {
+        LoginRequestDTO requestBody = new LoginRequestDTO("admin", "defaultPass");
         client.request()
-                .body(body)
+                .body(requestBody)
                 .when()
                 .post("/login")
                 .then()
                 .statusCode(200)
-                .body("$", Matchers.hasKey("id"));
+                .body(
+                        "size()", equalTo(5),
+                        "id", notNullValue(),
+                        "username", equalTo("admin"),
+                        "role", equalTo("admin"),
+                        "attempts", equalTo(0),
+                        "firstLogin", equalTo(false)
+                );
     }
+
+    @Test
+    @DisplayName("Login:: wrong credentials")
+    public void login_withWrongCredentials() {
+        LoginRequestDTO requestBody = new LoginRequestDTO("notAnUser", "wrongPassword");
+        client.request()
+                .body(requestBody)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(500)
+                .body(
+                        "size()", equalTo(2),
+                        "description", equalTo("User error"),
+                        "details", equalTo("Credenziali non riconosciute.")
+                );
+    }
+
+    @Test
+    @DisplayName("Login:: correct username and wrong password, locks an account after 4 times")
+    public void login_withWrongPasswordMultipleTimes_accountGetsLocked() {
+        LoginRequestDTO requestBody = new LoginRequestDTO("userToLock", "wrongPassword");
+        IntStream.rangeClosed(1, 4).forEach(repetition -> {
+            client.request()
+                    .body(requestBody)
+                    .when()
+                    .post("/login")
+                    .then()
+                    .statusCode(500)
+                    .body(
+                            "size()", equalTo(2),
+                            "description", equalTo("User error"),
+                            "details", equalTo("Credenziali non riconosciute.")
+                    );
+        });
+        client.request()
+                .body(requestBody)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(500)
+                .body(
+                        "size()", equalTo(2),
+                        "description", equalTo("User error"),
+                        "details", equalTo("Account bloccato, contatta l'amministratore per sbloccarlo.")
+                );
+
+    }
+
 }
